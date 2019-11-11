9ed88f1626b4df2047edc7bcda146e620178b3f2
Taylor Olson <pipestay622@gmail.com>
2019-11-10 22:55:03 -0800

    objects and arrays complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -87,6 +87,13 @@ const contactinfo = console.log(graduates.map(function(grad) {return `${grad.fir
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+for (i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.includes('Uni')) {
+    unisWithUni.push(graduates[i].university)
+  }
+
+  }
+  
 console.log(unisWithUni);
 
 
@@ -113,8 +120,10 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach((zooAnimals) => {
+  displayNames.push(`Name: ${zooAnimals.animal_name}, Scientific: ${zooAnimals.scientific_name}.`)
+});
 console.log(displayNames);
-
 /* Request 2: .map()
 
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
@@ -122,6 +131,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map((zooAnimals) => {
+  lowCaseAnimalNames.push(`${zooAnimals.animal_name.toLowerCase()}`)
+})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -130,15 +142,23 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
+zooAnimals.filter((zooAnimals) => {
+  if(zooAnimals.population < 5) {
+  lowPopulationAnimals.push(zooAnimals.animal_name)
+  }
+});
 console.log(lowPopulationAnimals);
-
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((accum, num) => {
+  return accum + num.population;
+}, 0);
+
 console.log(populationTotal);
+
 
 
 /*
